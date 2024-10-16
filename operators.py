15c3f07be694014092f0e6ce1b09a49c911b3a66
Alicjafir <alicja.firczyk@gmail.com>
2024-10-16 14:47:31 -0700

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,36 @@
+x = 9 #define x as a number 
+y = 3
+
+print("x+y = ", x+y)
+print("x-y = ", x-y)
+print("x*y = ", x*y)
+print("x/y = ", x/y)
+print("x%y = ", x%y) #Modulus
+print("x**y = ", x**y) #Exponentiation
+x = 9.191823
+print(x//y) #Floor division
+
+x=9 #x=9
+x +=3 #x=x+3
+print("x = 9; x+3; x = ", x)
+x = 9
+x -=3 #x=x-3
+print("x = 9; x-=3; x= ", x)
+x *=3 #x=x*3
+print("x *= 3; x= ", x)
+x /=3 #x=x/3
+print("x /= 3; x= ", x)
+x **=3 #x=x^3
+print("x *= 3; x= ", x)
+
+x = 9
+y = 3
+print("x==y ?", x==y) #True if x=y otherwise False
+print("x!=y ?", x!=y) #True if x doesn't equal y, False otherwise
+print("x>y ?", x>y) #True if x is greater than y
+print("x<y ?", x<y) #True if x is smaller than y
+print("x>=y ?", x>=y) #True if x is greater than or equal to y 
+print("x<=y ?", x<=y) #True if x is smaller or equal to y
+
+
+
