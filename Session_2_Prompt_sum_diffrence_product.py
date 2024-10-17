79434ae7b2c1f931a515211267dd9557b6cd6df3
Alicjafir <alicja.firczyk@gmail.com>
2024-10-16 22:17:01 -0700

    Add files via upload

---
+++ Session_2_Prompt_sum_diffrence_product.py
@@ -0,0 +1,19 @@
+
+x = 1.2
+float_number_1 = x
+
+y = 2.3
+float_number_2 = y 
+
+print("Sum of two floating numbers x+y =", x+y)
+
+a = 2 
+integer_1 = a
+
+b = 5 
+integer_2 = b 
+
+print("The diffrence between two integers a-b =", a-b)
+
+print("The prodact of the floating number x and integer a =", x*a)
+
