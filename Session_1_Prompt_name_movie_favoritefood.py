12fa7b9e2a87ff3c83b0df7a3578269b076399fa
Alicjafir <alicja.firczyk@gmail.com>
2024-10-16 20:38:01 -0700

    Add files via upload

---
+++ Session_1_Prompt_name_movie_favoritefood.py
@@ -0,0 +1,11 @@
+
+print("My name is Alicja Firczyk")
+
+print("My pronouns are she/her")
+
+print("My favorite food is steak. It is because you can comibine it with evrything. Also it has a lot of proteins which are good for the body.")
+
+print("My favorite movie is Twilight. It is because I watched it as kids with my sister and we loved it. Also it has a lot of parts so you can binge watch it for a long time.")
+
+
+
